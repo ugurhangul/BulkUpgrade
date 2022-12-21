59d298a6478c8df8a73bd8be8295fc79b8e89ac9
Adam Clauss <cabadam@gmail.com>
2022-12-21 11:45:37 -0600

    Initial commit.

---
+++ BulkRebuild/BulkRebuildMod.cs
@@ -0,0 +1,72 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace BulkRebuild
+{
+    public class BulkRebuildMod : MelonMod
+    {
+        private MelonPreferences_Category modCategory;
+        private MelonPreferences_Entry<KeyCode> triggerKey;
+        private GameManager gameManager;
+
+        /// <inheritdoc/>
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Loading configuration ...");
+            modCategory = MelonPreferences.CreateCategory("BulkRebuild");
+            modCategory.SetFilePath(Path.Combine(MelonUtils.UserDataDirectory, "BulkRebuild.cfg"));
+            triggerKey = modCategory.CreateEntry("TriggerKey", KeyCode.R, null, null, false, false, null, null);
+            LoggerInstance.Msg($"KeyCode is CTRL+{triggerKey.Value}");
+        }
+
+        /// <inheritdoc/>
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            try
+            {
+                if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl) || !Input.GetKeyDown(triggerKey.Value))
+                {
+                    return;
+                }
+
+                // Rebuild all buildings
+                BulkRebuild();
+            }
+            catch (Exception ex)
+            {
+                LoggerInstance.Error("Failed to rebuild buildings.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Executes rebuild operation on all ruins.
+        /// </summary>
+        private void BulkRebuild()
+        {
+            if (gameManager == null)
+            {
+                gameManager = UnityEngine.Object.FindObjectOfType<GameManager>();
+                if (gameManager != null)
+                {
+                    LoggerInstance.Msg($"Initialized GameManager");
+                }
+            }
+
+            if (gameManager != null)
+            {
+                LoggerInstance.Msg($"Searching for building ruins...");
+                var ruins = UnityEngine.Object.FindObjectsOfType<BuildingRuins>();
+                LoggerInstance.Msg($"Found {ruins.Count} ruins.");
+                foreach (var ruin in ruins)
+                {
+                    ruin.Rebuild();
+                }
+            }
+        }
+    }
+}
---
+++ BulkRebuild/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using BulkRebuild;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BulkRebuild")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BulkRebuild")]
+[assembly: AssemblyCopyright("Copyright © Cabadam 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("611bfd3e-fb6b-47be-9f68-d1f6ae76ffdc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: MelonInfo(typeof(BulkRebuildMod), "BulkRebuild", "0.1", "Cabadam")] //MelonInfo(typeof(ClassOfMod), “Game Mod Name”, “Version”, “Author”)
+[assembly: MelonGame("Crate Entertainment", "Farthest Frontier")] //MelonGame(“Studio”, “Game”)]
